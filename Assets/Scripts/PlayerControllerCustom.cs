000b1ffa647394d64d31a9867694a7268c7701f8
Carter David Henning <carterdavidhenning@gmail.com>
2021-06-04 17:23:10 -0400

    Feat: Finish no-physics movement
    To-do: add jumping and proper custom physics resolution.

--- Assets/Scripts/PlayerControllerCustom.cs
+++ Assets/Scripts/PlayerControllerCustom.cs
@@ -19,12 +19,15 @@ public class PlayerControllerCustom : MonoBehaviour
     public const string LEFT = "left";
 
     string buttonPressed;
-    bool isJumping;
+    [SerializeField]
+    bool isJumping = true;
+    Vector2 velocity;
 
     void Awake()
     {
         gravity = 2 * jumpHeight / (timeToJumpApex * timeToJumpApex);
         jumpVelocity = gravity * timeToJumpApex;
+        velocity = Vector2.zero;
     }
 
     void Update()
@@ -41,26 +44,47 @@ public class PlayerControllerCustom : MonoBehaviour
         {
             buttonPressed = null;
         }
-        Debug.Log(buttonPressed);
+
+        // Reenable once separation is coded
+        /*if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
+        {
+            isJumping = true;
+        }*/
     }
 
     private void FixedUpdate()
     {
+
         if (buttonPressed == RIGHT)
         {
-            transform.Translate(Vector2.right * (Time.fixedDeltaTime * moveSpeed), space);
+            velocity = Vector2.right * (Time.fixedDeltaTime * moveSpeed);
         }
         else if (buttonPressed == LEFT)
         {
-            transform.Translate(Vector2.left * (Time.fixedDeltaTime * moveSpeed), space);
+            velocity = -Vector2.right * (Time.fixedDeltaTime * moveSpeed);
         }
+        else
+        {
+            velocity = Vector2.zero;
+        }
+
+        // Reenable once separation is coded
+        /*if (isJumping)
+        {
+            velocity -= Vector2.up * (Time.fixedDeltaTime * gravity);
+        }*/
+
+        transform.Translate(velocity, space);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.tag == "Ground")
         {
+            velocity = new Vector2(velocity.x, 0f);
             isJumping = false;
+
+            // TODO: Resolve collision
         }
     }
 }
